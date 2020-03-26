003da7c321125348f6fa0f42e95b72583a7e5c96
10540015 <60342460+10540015@users.noreply.github.com>
2020-03-26 20:46:24 +0000

    Add files via upload

---
+++ Untitled2.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+shopping_list = ["banana","orange","apple","pear"]
+stock = {"banana":6,
+        "apple":0,
+        "orange":32,
+        "pear":15
+        }
+prices = {"banana":4,
+         "apple":2,
+         "orange":1.5,
+         "pear":3
+         }
+
